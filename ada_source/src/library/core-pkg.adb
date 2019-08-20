813b8317cf0112cc85ac9d55036d4fff74cf96de
jrmarino <draco@marino.st>
2019-08-20 15:01:59 -0500

    Reverse sort order
    We want to sort higher priority is first, so reverse "less than"
    function

--- ada_source/src/library/core-pkg.adb
+++ ada_source/src/library/core-pkg.adb
@@ -7,12 +7,14 @@ package body Core.Pkg is
    --------------------------------------------------------------------
    --  repo_priority_less_than
    --------------------------------------------------------------------
-   function repo_priority_less_than (A, B : T_repo_priority) return Boolean is
+   function repo_priority_less_than (A, B : T_repo_priority) return Boolean
+   is
+      --  Display 100 before 90, so it's reverse order (use greater than for "<")
    begin
       if A.priority = B.priority then
-         return SU."<" (A.reponame, B.reponame);
+         return SU.">" (A.reponame, B.reponame);
       else
-         return A.priority < B.priority;
+         return A.priority > B.priority;
       end if;
    end repo_priority_less_than;
 
