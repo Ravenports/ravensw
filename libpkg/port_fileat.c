0c0d23e50d2aa4987661254b046019fb612b4ce0
John Marino <draco@marino.st>
2020-06-02 10:24:08 -0500

    pull in port_fileat.c from master branch

--- libpkg/port_fileat.c
+++ libpkg/port_fileat.c
@@ -67,7 +67,7 @@ file_chdir_lock(int dfd)
 	file_at_dfd = dfd;
 
 	if (dfd == AT_FDCWD)
-		return 0;
+		return(0);
 
 	ret = fchdir(dfd);
 	if (ret != 0) {
@@ -88,7 +88,7 @@ file_chdir_unlock(int dfd)
 	file_at_dfd = -1;
 
 	if (dfd == AT_FDCWD)
-		return;
+		return(0);
 
 	if (saved_cwd[0] != '\0')
 		chdir(saved_cwd);
@@ -170,11 +170,15 @@ port_fchmodat(int fd, const char* path, mode_t mode, int flag)
 	if ((ret = file_chdir_lock(fd) != 0))
 		return(ret);
 
+#if defined(__sun__) || defined(__linux__)
+	ret = chmod(path, mode);
+#else
 	if (flag & AT_SYMLINK_NOFOLLOW) {
 		ret = lchmod(path, mode);
 	} else {
 		ret = chmod(path, mode);
 	}
+#endif
 
 	file_chdir_unlock(fd);
 	return(ret);
